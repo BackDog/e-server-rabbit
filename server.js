9f8e79ef39998eb5af0767884486df057aae2a48
Thanh Cảnh Nguyễn <canhnt1204@gmail.com>
2021-06-11 20:39:45 +0700

    s
    s

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ wss.on('connection', function connection(ws) {
     });
 });
 
+
+setInterval(() => {
+  wss.clients.forEach((client) => {
+    client.send('PING:' + Date.now());
+  });
+}, 5000);
+
 function sendAll (message) {
     for (var i=0; i<CLIENTS.length; i++) {
         CLIENTS[i].send(message);
