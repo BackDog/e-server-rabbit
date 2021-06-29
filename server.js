86b78aac76d5335e6510d36d07da2435630a0d63
Thanh Cảnh Nguyễn <canhnt1204@gmail.com>
2021-06-29 14:36:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var CLIENTS=[];
 wss.on('connection', function connection(ws) {
     CLIENTS.push(ws);
     ws.on('message', function incoming(message) {
-        sendAll(messagemy.replace(/<[^>]*>?/gm, ''););
+        sendAll(message.replace(/<[^>]*>?/gm, ''));
     });
 });
 
